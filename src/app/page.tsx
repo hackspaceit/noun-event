0dbedfaa16d0e8a529d5c1345b5965416bdcdc3b
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-07-14 14:33:57 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Html5Qrcode } from 'html5-qrcode';
-
+import {Html5QrcodeScanner} from "html5-qrcode";
+<script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
 export default function Page() {
   return (
     <div className="flex w-96 flex-col md:w-[600px]">
@@ -11,7 +12,7 @@ export default function Page() {
         <main className="flex flex-col space-x-4">
           <p className="text-body mb-4 text-white">
             Please scan the QR code for your wallet address that holds the NFT.
-          </p>
+                 </p>
         </main>
       </section>
     </div>
